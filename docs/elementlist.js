9d6b3bffb75db2da466b86c2bbece63eebc83176
Lalita <lalita@crazy-factory.co.th>
2018-07-27 10:57:08 +0700

    chore(docs): Remove

--- docs/elementlist.js
+++
@@ -1,2 +0,0 @@
-var ApiGen = ApiGen || {};
-ApiGen.elements = [{"file":"namespace-crazyfactory.boilerplate.html","label":"crazyfactory\\boilerplate"},{"file":"class-crazyfactory.boilerplate.Stack.html","label":"crazyfactory\\boilerplate\\Stack"}];
